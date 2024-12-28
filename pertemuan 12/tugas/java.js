d6a4c2ff59feb0469a65053e41de101abe684bec
Muhamad Fadil <dilmuh23@gmail.com>
2024-12-28 12:06:20 +0700

    Add files via upload

---
+++ pertemuan 12/tugas/java.js
@@ -0,0 +1,5 @@
+// Add any interactive features here
+document.addEventListener('DOMContentLoaded', function() {
+    // Example of a simple alert when the page loads
+    console.log("Welcome to Hotel XYZ!");
+});
